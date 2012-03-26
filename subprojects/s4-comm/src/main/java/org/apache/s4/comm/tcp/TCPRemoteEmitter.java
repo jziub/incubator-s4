8931c63c8957fcbd723cce6664d425d0ce3c6cfe
Matthieu Morel <mmorel@apache.org>
2012-03-26 15:46:20 +0200

    utility scripts + refactored twitter app - scripts for defining cluster configurations, starting nodes and uploading apps - removed custom event class for twitter app

--- subprojects/s4-comm/src/main/java/org/apache/s4/comm/tcp/TCPRemoteEmitter.java
+++ subprojects/s4-comm/src/main/java/org/apache/s4/comm/tcp/TCPRemoteEmitter.java
@@ -4,12 +4,15 @@ import org.apache.s4.base.RemoteEmitter;
 import org.apache.s4.comm.topology.RemoteTopology;
 
 import com.google.inject.Inject;
+import com.google.inject.name.Named;
 
 public class TCPRemoteEmitter extends TCPEmitter implements RemoteEmitter {
 
     @Inject
-    public TCPRemoteEmitter(RemoteTopology topology) throws InterruptedException {
-        super(topology);
+    public TCPRemoteEmitter(RemoteTopology topology, @Named("tcp.partition.queue_size") int bufferSize,
+            @Named("comm.retries") int retries, @Named("comm.retry_delay") int retryDelay,
+            @Named("comm.timeout") int timeout) throws InterruptedException {
+        super(topology, bufferSize, retries, retryDelay, timeout);
     }
 
 }
